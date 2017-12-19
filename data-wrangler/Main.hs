7622d5ddadc747efa04dd4cc73ec9181f75643eb
Rory Dungan <rorydungan@gmail.com>
2017-12-19 19:42:55 +1000

    Worked on code for downloading charts info

--- data-wrangler/Main.hs
+++ data-wrangler/Main.hs
@@ -7,9 +7,18 @@ import Network.Wreq
 import Control.Lens
 import qualified Data.ByteString.Lazy.Char8 as C
 
+countries = ["us", "gb"]
+-- , "ad", "ar", "at", "au", "be", "bg", "bo", "br", "ca",
+--              "ch", "cl", "co", "cr", "cy", "cz", "de", "dk", "do", "ec", "ee",
+--              "es", "fi", "fr", "gr", "gt", "hk", "hn", "hu", "id", "ie", "is",
+--              "it", "jp", "lt", "lu", "lv", "mc", "mt", "mx", "my", "ni", "nl",
+--              "no", "nz", "pa", "pe", "ph", "pl", "pt", "py", "se", "sg", "sk",
+--              "sv", "th", "tr", "tw", "uy"]
+
+
 main :: IO ()
 main = do
-    let url = "http://httpbin.org/get"
-    putStrLn $ "GET " ++ url
-    r <- get url
-    C.putStrLn (r ^. responseBody)
+    let url = "https://spotifycharts.com/regional/"
+    let urls = map (\c -> url ++ c ++ "/daily/latest/download") countries
+    reqs <- mapM get urls
+    mapM_ (\r -> C.putStrLn (r ^. responseBody)) reqs
