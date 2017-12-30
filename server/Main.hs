9f3328a6668fc05668a7ea5aea480aa8a4ede913
Rory Dungan <rorydungan@gmail.com>
2017-12-30 23:46:07 +1000

    Added route to server for getting all artists. TODO: return JSON

--- server/Main.hs
+++ server/Main.hs
@@ -1,15 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+
 module Main where
 
-import Lib
 import Web.Scotty
 import Network.Wai.Middleware.Static
-import Data.Aeson (FromJSON, ToJSON)
+--import Data.Aeson (FromJSON, ToJSON)
+import Data.Bson (ObjectId())
+import Database.MongoDB (Action, Document, Pipe, Query, master, connect, host, 
+                         access, close, find, select, project, rest, (=:))
+import Database.MongoDB.Query (Collection)
+import qualified Data.Text.Lazy as T
+
+statsCollection :: Collection
+statsCollection = "stats"
 
 main :: IO ()
-main = scotty 3000 $ do
-    get "/test" $
-        text "Hello world"
+main = do 
+    pipe <- connect (host "localhost")
 
-    middleware $ staticPolicy (noDots >-> addBase "frontend")
+    scotty 3000 $ do
+
+        get "/artists" $ do
+            res <- allArtists pipe
+            text $ T.pack $ show res
+
+        middleware $ staticPolicy (noDots >-> addBase "frontend")
+
+    close pipe
+
+runQuery :: Pipe -> Query -> ActionM [Document]
+runQuery pipe query = access pipe master "music-map" (find query >>= rest)
+
+allArtists :: Pipe -> ActionM [Document]
+allArtists pipe = 
+    runQuery pipe (select [] statsCollection) { project = ["artistName" =: 1] }
+
+artistStats :: Pipe -> ObjectId -> ActionM [Document]
+artistStats pipe oid = 
+    runQuery pipe (select ["_id" =: oid] statsCollection)
