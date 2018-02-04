1ba230da0e390ac0e4cc9837b182c515cbbf8708
Rory Dungan <rorydungan@gmail.com>
2018-02-04 16:09:24 +1000

    Added some more comments because I forgot what certain functions did

--- src/DataProcessor.hs
+++ src/DataProcessor.hs
@@ -21,6 +21,12 @@ import qualified Data.Csv as Cassava
 -- vector
 import Data.Vector (Vector)
 
+{-| 
+  Takes a country code and, a list of track entires (track name, artist name, 
+  and number of streams), and returns a list of country entries (mapping of a 
+  country code, artist name, and the percentage of streams in the top 200 songs
+  for that country that the artist has).
+-}
 processData :: CountryTitle -> [Track] -> [CountryEntry]
 processData c xs =
     let countryTotal = fromIntegral $
@@ -31,6 +37,11 @@ processData c xs =
     & groupBy (\(Country n1 a1 _) (Country n2 a2 _) -> n1 == n2 && a1 == a2)
     & map (foldl1' (\(Country n a p1) (Country _ _ p2) -> Country n a (p1 + p2)))
 
+{-|
+  Takes a list of country entries and groups entries for the same artist together
+  so that we get an object with the artist name and a list of their streams
+  for each country. 
+-}
 artistSummaries :: [CountryEntry] -> [ArtistEntry]
 artistSummaries xs =
     sortBy (\(Country _ a1 _) (Country _ a2 _) -> a1 `compare` a2) xs
@@ -44,6 +55,7 @@ artistSummaries xs =
         in  Artist name streamsPerCountry
     )
 
+-- |Read top 200 track charts from CSV data
 decodeItems :: ByteString -> Either String (Vector Track)
 decodeItems = fmap snd . Cassava.decodeByName
 
