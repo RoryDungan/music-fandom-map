dc66464b24520bcc99a244eaa4f01ba392e4164e
Rory Dungan <rorydungan@gmail.com>
2018-02-08 00:12:00 +1000

    Added some helper functions for dealing with artist summaries

--- src/ArtistInfo.hs
+++ src/ArtistInfo.hs
@@ -13,6 +13,8 @@ module ArtistInfo
     , ArtistBio
     , summary 
     , decodeArtistInfo
+    , largeImageURL
+    , extractBio
     ) where 
 
 import GHC.Generics
@@ -23,10 +25,12 @@ import Data.Aeson (FromJSON, Value, eitherDecode, parseJSON, withObject, (.:))
 import Data.Aeson.Types (Parser, parseEither)
 
 import Data.Text (Text)
+import qualified Data.Text as T
 
 import Data.ByteString.Lazy (ByteString)
 
-import Data.Vector (Vector)
+import Data.Vector (Vector, (!?))
+import qualified Data.Vector as V
 
 data ArtistSummary = ArtistSummary 
     { name :: Text
@@ -51,6 +55,7 @@ data ArtistBio = ArtistBio
     } deriving (Show, Generic)
 instance FromJSON ArtistBio where
 
+-- |Decode an ArtistSummary from JSON
 decodeArtistInfo :: ByteString -> Either String ArtistSummary
 decodeArtistInfo b = 
     case eitherDecode b of 
@@ -67,3 +72,13 @@ parseArtistInfo = withObject "tuple" $ \obj -> do
         Nothing -> case err of 
             Just e  -> Left e
             Nothing -> Left "Error parsing artist"
+
+-- |Extract the large image URL from the specified summary
+largeImageURL :: ArtistSummary -> Maybe Text 
+largeImageURL (ArtistSummary _ images _) = 
+    let extralarge = V.filter (\i -> (size i) == T.pack "extralarge") images
+    in  imageUrl <$> extralarge !? 0 
+
+-- |Extract just the biography section from the specified artist summary
+extractBio :: ArtistSummary -> Text 
+extractBio (ArtistSummary _ _ b) = summary b
